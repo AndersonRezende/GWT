ab1cd91fdf2137fb29f816d965a81bd617f3b1b4
Anderson Rezende <andersonrezende17@hotmail.com>
2025-03-05 23:43:06 -0300

    Improving mocking

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"HelloWorld/mocking"
 	"os"
+	"time"
 )
 
 func main() {
-	sleeper := &mocking.DefaultSleeper{}
+	mock()
+}
+
+func mock() {
+	/*sleeper := &mocking.DefaultSleeper{}
+	mocking.Countdown(os.Stdout, sleeper)*/
+
+	sleeper := &mocking.ConfigurableSleeper{1 * time.Second, time.Sleep}
 	mocking.Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown.go
+++ mocking/countdown.go
@@ -27,6 +27,39 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+const write = "write"
+const sleep = "sleep"
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	Duration time.Duration
+	SleepF   func(duration time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.SleepF(c.Duration)
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
